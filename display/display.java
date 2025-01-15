b24389d01ffa62f95fd920fd7a086ad68ac0754b
steph-leo <leostephs89@gmail.com>
2025-01-15 11:33:37 +0300

    Add file

---
+++ display/display.java
@@ -0,0 +1,13 @@
+public class display {
+
+    // Method without parameters and return type
+    public static void display() {
+        System.out.println("Hello Said to the screen");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Calling the display method
+        display();
+    }
+}
